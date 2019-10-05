c0d5fd0fd726e75d59da53b9093b23e5813e7b14
jwz2017 <30640406+jwz2017@users.noreply.github.com>
2019-10-05 10:38:59 +0800

    1
    11

--- include/1superClick.js
+++ include/1superClick.js
@@ -17,7 +17,7 @@ class Main extends GFrame {
         super(canvasId);
         
         /*********自适应*********** */
-        this.adapt();
+        this.adapt(false);
 
         /*********预加载手动********** */
         // this.preload([{
