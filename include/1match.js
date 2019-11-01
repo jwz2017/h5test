ea350af53501e19161c364df57cdb5d4bade70f7
jwz2017 <30640406+jwz2017@users.noreply.github.com>
2019-11-01 13:51:28 +0800

    qw
    www

--- include/1match.js
+++ include/1match.js
@@ -63,7 +63,7 @@ class Main extends GFrame {
         mc.style.fontSize = 30; //按钮label字体大小
 
         this.titleScreen = new BasicScreen();
-        this.titleScreen.createDisplayText('开始界面02', width / 2, 200);
+        this.titleScreen.createDisplayText('开始界面03', width / 2, 200);
         this.titleScreen.createOkButton((width - 200) / 2, height / 2 + 100, 'start', 200, 40);
         // this.titleScreen=new lib.Title();//协作animate使用-------------------1
 
@@ -259,7 +259,7 @@ class Main extends GFrame {
 class Card extends createjs.Container{
     constructor(face,card="card",back="back"){
         super();
-        // this.shadow=new createjs.Shadow("#333",3,3,5);
+        this.shadow=new createjs.Shadow("#333",3,3,5);
         this.back=new createjs.Bitmap(queue.getResult("card"));//使用queue,不能clone   使用地址不能用image.width.要直接用数字
         this.regX=this.back.image.width/2;
         this.regY=this.back.image.height/2;
